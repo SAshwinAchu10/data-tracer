a78511f20d5041b332cf540e4aec634a9011cf09
Ashwin <achu10@live.in>
2020-02-18 23:32:43 +0530

    example code usage added

---
+++ examples/javascript/app.js
@@ -0,0 +1,48 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let Audit = require('data-tracer')
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// initializing tracer
+Audit.addTracer('mongoose', { connectionString: 'mongodb://localhost:27017/ashwin' })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.get('/ping', function (req, res) {
+
+    Audit.logEvent(
+        'INFO',
+        'Ping',
+        'api was triggered',
+        'Successful',
+        undefined,
+        'app.js',
+        'to Check Server status',
+        'Event',
+        {}
+    )
+    res.json({ message: 'pong' })
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ examples/typescript/app.ts
@@ -0,0 +1,44 @@
+import * as express from 'express';
+import { Request, Response } from 'express';
+import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+let Audit = require('data-tracer')
+
+
+const port = process.env.PORT || 3000;
+const app: express.Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+Audit.addTracer('mongoose', { connectionString: 'mongodb://localhost:27017/ashwin' })
+
+
+app.use(
+    '/',
+    (req: Request, res: Response): Response => {
+        return res.status(200).json('Hello World');
+    }
+);
+
+app.get('/ping', function (req, res) {
+
+    Audit.logEvent(
+        'INFO',
+        'Ping',
+        'api was triggered',
+        'Successful',
+        undefined,
+        'app.js',
+        'to Check Server status',
+        'Event',
+        {}
+    )
+    res.json({ message: 'pong' })
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`);
+});
